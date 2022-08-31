e67cf0359b1e12ea8e6a7f95751dab984bbb8b64
Patrick Knight <pfknight2@gmail.com>
2022-08-31 11:33:16 -0400

    lesson part2 done

--- tunr_project/tunr_django/settings.py
+++ tunr_project/tunr_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tunr'
+    'tunr',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,14 @@ DATABASES = {
         'PASSWORD': 'tunr',
         'HOST': 'localhost'
     }
+}
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 # Password validation
